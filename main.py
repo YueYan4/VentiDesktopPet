6c493a2391cdf12104756451bb28870ab25af2b1
YueYan4 <yueyan@email.unc.edu>
2022-09-03 18:04:08 -0400

    fixed menu showing up and dragging; still need to make bg disappear

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+from turtle import bgcolor
 from keep_alive import keep_alive
 
 import random
 import tkinter as tk
 import time
+from tkinter import *
 #assets from https://www.reddit.com/r/Genshin_Impact/comments/oq2w62/i_made_a_venti_shimeji/
-x = 2560
-y = 1600
+x = 400
+y = 635
 cycle = 0
 check = 1
 idle_num =[1,2,3,4]
 sleep_num = [10,11,12,13,15]
 walk_left = [6,7]
 walk_right = [8,9]
-flying_arr = [16,17]
+flying_arr = [16,17, 18]
 event_number = random.randrange(1,17,1)
 lastClickX = 0
 lastClickY = 0
@@ -23,55 +25,42 @@ def saveLastClickPos(event):
       lastClickY = event.y
       
 def dragging(event):
-      a, b = event.x - lastClickX + window.winfo_x(), event.y - lastClickY + window.winfo_y()
-      window.geometry("+%s+%s" % (a , b))
+      global lastClickX, lastClickY, x, y
+      x, y = event.x - lastClickX + window.winfo_x(), event.y - lastClickY + window.winfo_y()
+      window.geometry("+%s+%s" % (x , y))
 
 #impath = 'C:\\Users\\fx770\\Desktop\\Project\\Buddy\\image\\'
 #transfer random no. to event
-def event(cycle,check,event_number,x):
+def event(cycle,check,event_number):
+ global x, y
  if event_number in idle_num:
   check = 0
   #print('idle')
-  window.after(400,update,cycle,check,event_number,x) #no. 1,2,3,4 = idle
+  window.after(400,update,cycle,check,event_number) #no. 1,2,3,4 = idle
  elif event_number == 5:
   check = 1
   #print('from idle to sleep')
-  window.after(100,update,cycle,check,event_number,x) #no. 5 = idle to sleep
+  window.after(100,update,cycle,check,event_number) #no. 5 = idle to sleep
  elif event_number in walk_left:
   check = 4
   #print('walking towards left')
-  window.after(100,update,cycle,check,event_number,x)#no. 6,7 = walk towards left
+  window.after(100,update,cycle,check,event_number)#no. 6,7 = walk towards left
  elif event_number in walk_right:
   check = 5
   #print('walking towards right')
-  window.after(100,update,cycle,check,event_number,x)#no 8,9 = walk towards right
+  window.after(100,update,cycle,check,event_number)#no 8,9 = walk towards right
  elif event_number in sleep_num:
   check  = 2
   #print('sleep')
-  window.after(1000,update,cycle,check,event_number,x)#no. 10,11,12,13,15 = sleep
+  window.after(100,update,cycle,check,event_number)#no. 10,11,12,13,15 = sleep
  elif event_number == 14:
   check = 3
   #print('from sleep to idle')
-  window.after(100,update,cycle,check,event_number,x)#no. 15 = sleep to idle
+  window.after(100,update,cycle,check,event_number)#no. 15 = sleep to idle
  elif event_number in flying_arr:
   check = 6
-  window.after(100,update,cycle,check,event_number,x)
+  window.after(100,update,cycle,check,event_number)
 #making gif work 
-
-def start_move(event, cycle, event_number):
-      label.place(relx = event.x, rely = event.y) 
-      cycle ,event_number = gif_work(cycle,flying,event_number,1,9)
-
-def stop_move(event):
-      label.place(relx = None, rely = None)
-
-def do_move(event, cycle, event_number):
-      deltax = event.x - label.winfo_x()
-      deltay = event.y - label.winfo_x()
-      x = label.winfo_x() + deltax
-      y = label.winfo_y() + deltay
-      window.geometry(f"+{x}+{y}")
-      cycle ,event_number = gif_work(cycle,hang,event_number,1,9)
 
 def gif_work(cycle,frames,event_number,first_num,last_num):
  if cycle < len(frames) -1:
@@ -81,9 +70,9 @@ def gif_work(cycle,frames,event_number,first_num,last_num):
   event_number = random.randrange(first_num,last_num+1,1)
  return cycle,event_number
 
-def update(cycle,check,event_number,x):
+def update(cycle,check,event_number):
+ global x, y
  #idle
- global y
  if check ==0:
   frame = idle[cycle]
   cycle ,event_number = gif_work(cycle,idle,event_number,1,9)
@@ -100,31 +89,38 @@ def update(cycle,check,event_number,x):
  elif check ==3:
   frame = sleep_to_idle[cycle]
   cycle ,event_number = gif_work(cycle,sleep_to_idle,event_number,1,1)
+  x += 3
 #walk toward left
  elif check == 4:
   frame = walk_positive[cycle]
   cycle , event_number = gif_work(cycle,walk_positive,event_number,1,9)
-  x -= 3
-  window.geometry('+'+str(x)+'+1050')#resolution
+  x += 3
+  #window.geometry(f"+{x}+{y}")
 #walk towards right
  elif check == 5:
   frame = walk_negative[cycle]
   cycle , event_number = gif_work(cycle,walk_negative,event_number,1,9)
-  x -= -3
-  window.geometry('+'+str(x)+'+1050')#resolution
+  x -= 3
+  #window.geometry(f"+{x}+{y}")
  elif check == 6:
   frame = flying[cycle]
-  cycle,event_number = gif_work(cycle, flying, event_number, 1, 3)
-  y -= 3
-  
- window.tk.call('wm', 'overrideredirect', window._w, True)
- #window.geometry("+%s+%s" % (x, window.winfo_y()))
- label.configure(image=frame)
- window.after(1,event,cycle,check,event_number,x)
+  cycle,event_number = gif_work(cycle, flying, event_number, 1, 9)
+  r = random.randint(0, 1)
+  if r == 0:
+      y -= 6
+  elif r == 1:
+      y += 6
+  #window.geometry(f"+{x}+{y}")
+ #window.tk.call('wm', 'overrideredirect', window._w, True)
+ window.geometry('+' + str(x)+'+' + str(y))
+ label.config(image=frame)
+ window.after(1,event,cycle,check,event_number)
 
 
 window = tk.Tk()
 window.tk.call('wm', 'overrideredirect', window._w, True)
+window.geometry("+{}+{}".format(x, y))
+#window.tk.call('wm', 'overrideredirect', window._w, True)
 #call buddy's action gif
 idle = [tk.PhotoImage(file='idle.gif',format = 'gif -index %i' %(i)) for i in range(3)]#idle gif
 idle_to_sleep = [tk.PhotoImage(file='idle_to_sleep.gif',format = 'gif -index %i' %(i)) for i in range(5)]#idle to sleep gif
@@ -136,13 +132,17 @@ flying = [tk.PhotoImage(file='flying.gif',format = 'gif -index %i' %(i)) for i i
 hang = [tk.PhotoImage(file='hang.gif',format = 'gif -index %i' %(i)) for i in range(6)]#walk to right gif
 
 #window configuration
-window.config(highlightbackground='black')
+#window.config(highlightbackground='black')
 #window.overrideredirect(True)
 
+window.wm_attributes('-topmost', 'true')
+window.wm_attributes('-topmost', 'true')
+window.wm_attributes('-transparent', True)
+window.config(bg='systemTransparent')
 
-window.wm_attributes('-topmost', True)
 window.bind('<Button-1>', saveLastClickPos)
 window.bind('<B1-Motion>', dragging)
+
 
 #window.wm_attributes('-transparent',True)
 #window.wm_attributes('transparentcolor', 'black')
@@ -151,16 +151,13 @@ window.bind('<B1-Motion>', dragging)
 
 #window.geometry('100x100+'+str(x)+'+1050')
 #window.wm_attributes('-alpha',0.3)
-label = tk.Label(window,bd=0)
-#label.config(bg='systemTransparent')
+label = tk.Label(window)
 label.pack()
-#label.bind("<ButtonPress-1>", lambda event: start_move(event, cycle, event_number))
-#label.bind("<ButtonRelease-1>", lambda event: stop_move(event))
-#label.bind("<B1-Motion>", lambda event: do_move(event, cycle, event_number))
-#window.lift()
-#keep_alive()
+
 #loop the program
-window.after(1,update,cycle,check,event_number,x)
+window.after(1,update,cycle,check,event_number)
+#window.tk.call('wm', 'overrideredirect', window._w, True)
+
 window.mainloop()
 #https://ezgif.com/maker
 #Draw venti walking, sleeping, throwing flowers, flying, turning into wisp
